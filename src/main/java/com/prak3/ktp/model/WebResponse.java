600f623e2bb3205c5205079fd28ad01558e6fdb1
Putranugroho14 <putranugroho61@gmail.com>
2026-03-17 15:33:37 +0700

    Membuat wrapper standar WebResponse API

---
+++ src/main/java/com/prak3/ktp/model/WebResponse.java
@@ -0,0 +1,16 @@
+package com.prak3.ktp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
